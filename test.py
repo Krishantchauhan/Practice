ff6e21cef1c3a6f4e08be970b59462a4244dc5b1
Krishantchauhan <krashantchauhan@gmail.com>
2023-11-14 15:41:18 +0530

    test
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- test.py
+++ test.py
@@ -1,1 +1,22 @@
-print("test")
+# print("test")
+
+
+def can_sort_array(n, arr):
+    if sorted(arr) == arr:
+        return "YES"
+
+    m = 1
+    while m * 2 <= n:
+        m *= 2
+
+    if m == n:
+        return "NO"
+
+    return "NO"
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = can_sort_array(n, arr)
+    print(result)
