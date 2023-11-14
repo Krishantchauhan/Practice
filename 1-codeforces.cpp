d9906e6f25435261bd92d3885b68f56b58622a9d
Krishantchauhan <krashantchauhan@gmail.com>
2023-11-14 15:41:00 +0530

    codeforces
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- 1-codeforces.cpp
+++ 1-codeforces.cpp
@@ -1,61 +1,122 @@
 #include <iostream>
-// #include <algorithm>
+#include <vector>
+#include <algorithm>
+#include <numeric>
 #include <map>
+#include <unordered_map>
+#include <queue>
+#include <set>
 
 // #include <bits/stdc++.h>
 using ll = long long;
 
-#define loop(n) for (int i = 0; i < n; i++)
+#define loop(n) for (ll i = 0; i < n; i++)
+#define loop2(n) for (ll i = n - 1; i >= 0; i--)
 
 ll ct = 0;
 ll low = 0, high = 0;
 ll n, k;
 ll N;
-int flag = 1;
+int flag = 0;
 ll sum = 0;
 
 using namespace std;
 
-void ptr(ll arr[], ll low, ll high)
+void Sorting_with_Twos()
 {
-    ct = 0;
-    while (low < high)
+    ll first;
+    cin >> n >> first;
+
+    ll temp;
+    flag = 0;
+    for (ll i = 1; i < n; i++)
     {
-        while (arr[low] == 0 && low < high)
-            low++;
-        while (arr[high] == 1 && high > (low + high) / 2)
-            high--;
-        if (low < high)
+        cin >> temp;
+        if (first > temp)
         {
-            ct++;
-            swap(arr[low], arr[high]);
+            if (((i - 1) & i) != 0)
+                flag = -1;
         }
+        first = temp;
     }
-    cout << ct;
+
+    if (flag)
+        cout << "NO"
+             << "\n";
+    else
+        cout << "YES"
+             << "\n";
 }
 
-void Sum_of_Three(ll n)
+void Deja_Vu()
 {
-    if (n <= 6)
-        cout << "NO" << endl;
-    else
+    int n, q;
+    cin >> n >> q;
+
+    vector<int> ans(n), a;
+
+    loop(n)
+            cin >>
+        ans[i];
+
+    loop(q)
     {
-        int a = 1;
-        int b = 2;
-        int c = n - a - b;
-        if (c % 3 == 0)
+        int temp;
+        cin >> temp;
+
+        if (a.empty() || a.back() > temp)
+            a.push_back(temp);
+    }
+
+    loop(n)
+    {
+        for (int j = 0; j < a.size(); j++)
         {
-            b += 2;
-            c -= 2;
-        }
-        if (c <= b)
-            cout << "NO" << endl;
-        else
-        {
-            cout << "YES" << endl;
-            cout << a << " " << b << " " << c << endl;
+            if (ans[i] % (1 << a[j]) == 0)
+                ans[i] |= 1 << (a[j] - 1);
         }
     }
+
+    loop(n)
+            cout
+        << ans[i] << " ";
+
+    cout << "\n";
+}
+
+void Smilo_and_Monsters()
+{
+    ll n, temp = 0, x = 0, mn = 0;
+    sum = 0;
+    cin >> n;
+    vector<ll> ans(n);
+
+    loop(n)
+    {
+        cin >> ans[i];
+        sum += ans[i];
+    }
+
+    sort(ans.begin(), ans.end());
+    reverse(ans.begin(), ans.end());
+
+    mn = sum;
+
+    loop(n)
+    {
+        temp += ans[i];
+        sum -= ans[i];
+        if (temp > sum)
+        {
+            x = temp - sum;
+            x = (x + 1) / 2;
+        }
+        ll res = sum + x;
+        mn = min(mn, i + 1 + res);
+        x = 0;
+    }
+
+    cout << mn << "\n";
 }
 
 int main()
@@ -64,29 +125,9 @@ int main()
     cin >> N;
     while (N--)
     {
-        cin >> n;
-
-        ll arr[n], b[n];
-
-        // for (ll i = 0; i < n; i++)
-        // {
-        //     cin >> arr[i];
-        //     sum += abs(arr[i]);
-        //     b[i] = arr[i];
-        // }
-        // cin>>k;
-
-        // sort(arr, arr + n);
-
-        // ptr(arr, 0, n - 1);
-        // Sum_of_Three(n);
-
-        loop(5)
-        {
-            cout << i;
-        }
-
-        cout << endl;
+        // Sorting_with_Twos();
+        // Deja_Vu();
+        Smilo_and_Monsters();
     }
     return 0;
 }
